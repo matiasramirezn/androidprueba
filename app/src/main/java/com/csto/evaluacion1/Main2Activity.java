f0b107a3b9ad8516247d1605c4b59d0a65b20dad
matias <matias.ramirez.n95@gmail.com>
2021-05-31 19:40:47 -0400

    s

--- app/src/main/java/com/csto/evaluacion1/Main2Activity.java
+++ app/src/main/java/com/csto/evaluacion1/Main2Activity.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
-//algo sa
+//algo sasa
 public class Main2Activity extends AppCompatActivity {
 
     EditText nombre;
